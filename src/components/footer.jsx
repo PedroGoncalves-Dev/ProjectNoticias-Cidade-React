c4f0cd01bee668d24dbb02813a41be8f271dd378
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-07-24 19:32:37 -0300

    criando o footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+
+    return (
+       <footer>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>Quem somos</Link>
+                        <Link to='/'>Contatos</Link>
+                        <Link to='/'>Nos ajude com as notícias</Link>
+                    </li>
+                </ul>
+            </div>
+            <h5>&copy; Copyright 2000-2018 Notícias cidade</h5>
+       </footer>
+    )
+}
